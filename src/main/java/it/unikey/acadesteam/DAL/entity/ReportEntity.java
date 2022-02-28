97c5a12389b1ef3e337adf13f8a96ab356f4cedd
silins <silvia.inserra@outlook.it>
2022-02-28 17:39:12 +0100

    first dal commit

--- src/main/java/it/unikey/acadesteam/DAL/entity/ReportEntity.java
+++ src/main/java/it/unikey/acadesteam/DAL/entity/ReportEntity.java
@@ -1,4 +1,14 @@
 package it.unikey.acadesteam.DAL.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class ReportEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }

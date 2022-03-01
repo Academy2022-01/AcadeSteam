c37c0dc56c30562fd2c17546e2b625d9c17e1cf0
DamianoLandi <damianolandi01@gmail.com>
2022-03-01 13:04:06 +0100

    sus

---
+++ src/main/java/it/unikey/acadesteam/BLL/dto/GameDTO.java
@@ -0,0 +1,17 @@
+package it.unikey.acadesteam.BLL.dto;
+
+import it.unikey.acadesteam.DAL.entity.GameStateEntity;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+
+public class GameDTO {
+    private UUID id;
+    private String gameTitle;
+    private String gamePath;
+    private GameStateEntity gameState;
+//    private UserInfoEntity developer;
+
+}

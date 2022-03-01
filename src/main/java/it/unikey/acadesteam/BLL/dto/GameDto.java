9c0898205073eb1e7886755c38b1b1590b693b26
La_Romanina <marziabragaglia@gmail.com>
2022-03-01 17:33:10 +0100

    "GameController final"

--- src/main/java/it/unikey/acadesteam/BLL/dto/GameDto.java
+++ src/main/java/it/unikey/acadesteam/BLL/dto/GameDto.java
@@ -3,15 +3,13 @@ package it.unikey.acadesteam.BLL.dto;
 import it.unikey.acadesteam.DAL.entity.GameStateEntity;
 import lombok.Data;
 
-import java.util.UUID;
-
 @Data
 
 public class GameDto {
-    private UUID id;
+    private Integer id;
     private String gameTitle;
     private String gamePath;
     private GameStateEntity gameState;
-//    private UserInfoEntity developer;
+    private UserInfoDto developer;
 
 }
--- src/main/java/it/unikey/acadesteam/PL/rest/GameRest.java
+++ src/main/java/it/unikey/acadesteam/PL/rest/GameRest.java
@@ -1,2 +1,11 @@
-package it.unikey.acadesteam.PL.rest;public class GameRest {
+package it.unikey.acadesteam.PL.rest;
+
+import it.unikey.acadesteam.DAL.entity.GameStateEntity;
+
+public class GameRest {
+    private Integer id;
+    private String gameTitle;
+    private String gamePath;
+    private GameStateEntity gameState;
+    //private UserInfoRest developer;
 }

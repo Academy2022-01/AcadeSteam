200a122a2adc5e6e0124070e3f97c99a19bab426
AlessioPaolucci <ale.paolucci3@stud.uniroma3.it>
2022-03-01 18:05:30 +0100

    finalmente il rest è fatto

--- src/main/java/it/unikey/acadesteam/PL/rest/ReportRest.java
+++ src/main/java/it/unikey/acadesteam/PL/rest/ReportRest.java
@@ -1,4 +1,21 @@
 package it.unikey.acadesteam.PL.rest;
 
+import it.unikey.acadesteam.BLL.dto.GameDto;
+import it.unikey.acadesteam.BLL.dto.ReportTypeDto;
+import it.unikey.acadesteam.BLL.dto.UserInfoDto;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@RequiredArgsConstructor
 public class ReportRest {
+    private UUID id;
+    private LocalDate date;
+    private String description;
+    private ReportTypeRest reportType;
+    //private GameRest game;
+    //private UserInfoRest userInfo;
 }

26695f2252f253162a18912d4b95d0affb5d926b
Roby <vaiaroberta@gmail.com>
2022-03-01 11:32:01 +0100

    LibraryEntity e UserInfoEntity

--- src/main/java/it/unikey/acadesteam/DAL/entity/LibraryEntity.java
+++ src/main/java/it/unikey/acadesteam/DAL/entity/LibraryEntity.java
@@ -1,2 +1,36 @@
-package it.unikey.acadesteam.DAL.entity;public class LibraryEntity {
+package it.unikey.acadesteam.DAL.entity;
+
+import lombok.Data;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name= "LIBRARY")
+public class LibraryEntity {
+
+
+    @Id
+    @Type(type = "uuid-binary")
+    @GeneratedValue(generator = "myGUID")
+    @GenericGenerator(name = "myGUID", strategy = "uuid2")
+    @Column(name = "ID", length = 16, unique= true, nullable = false)
+    private UUID id;
+
+
+    @OneToMany(mappedBy = "gameEntity",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<GameEntity> game;
+
+    @Column(name = "USERS_INFO_ID", nullable = false)
+    @OneToOne(fetch = FetchType.LAZY)
+    private UserInfoEntity usersInfo;
+
+
+
+
+
 }

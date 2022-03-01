449ffe6497a0adbd42a7b345493d24fd7a8e899e
LAPTOP-AD1HIVPQ\ACER <alessio.giannetti.gnn@gmail.com>
2022-03-01 10:34:46 +0100

    DAL/user_roles

--- src/main/java/it/unikey/acadesteam/DAL/repository/UserRepository.java
+++ src/main/java/it/unikey/acadesteam/DAL/repository/UserRepository.java
@@ -4,7 +4,9 @@ import it.unikey.acadesteam.DAL.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
 }

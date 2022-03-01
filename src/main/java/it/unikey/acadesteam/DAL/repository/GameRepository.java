03fbd6355e5d057803ea0e650f165c86d28bd130
DamianoLandi <damianolandi01@gmail.com>
2022-03-01 09:08:32 +0100

    team 3 first commit

---
+++ src/main/java/it/unikey/acadesteam/DAL/repository/GameRepository.java
@@ -0,0 +1,11 @@
+package it.unikey.acadesteam.DAL.repository;
+
+import it.unikey.acadesteam.DAL.entity.GameEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface GameRepository extends JpaRepository<GameEntity, UUID> {
+}

f67e92d09f9e52eace24def040ce978d9169d643
MonarkOfTheHunt <jonathanmosetti@gmail.com>
2022-03-01 11:40:37 +0100

    update reposotory

--- src/main/java/it/unikey/acadesteam/DAL/repository/LibraryRepository.java
+++ src/main/java/it/unikey/acadesteam/DAL/repository/LibraryRepository.java
@@ -1,5 +1,6 @@
 package it.unikey.acadesteam.DAL.repository;
 
+import it.unikey.acadesteam.DAL.entity.LibraryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
--- src/main/java/it/unikey/acadesteam/DAL/repository/UserInfoRepository.java
+++ src/main/java/it/unikey/acadesteam/DAL/repository/UserInfoRepository.java
@@ -1,4 +1,9 @@
 package it.unikey.acadesteam.DAL.repository;
 
-public interface UserInfoRepository JpaRepository<UserInfoEntity, UUID>{
+import org.springframework.data.jpa.repository.JpaRepository;
+import it.unikey.acadesteam.DAL.entity.UserInfoEntity;
+
+import java.util.UUID;
+
+public interface UserInfoRepository extends JpaRepository<UserInfoEntity, UUID> {
 }

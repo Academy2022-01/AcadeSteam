3481602c597e117ad58ba7460b3c5c9c2ccde66d
LAPTOP-AD1HIVPQ\ACER <alessio.giannetti.gnn@gmail.com>
2022-03-01 11:06:20 +0100

    DAL/user_roles

--- src/main/java/it/unikey/acadesteam/DAL/entity/RolesEntity.java
+++ src/main/java/it/unikey/acadesteam/DAL/entity/RolesEntity.java
@@ -3,16 +3,20 @@ package it.unikey.acadesteam.DAL.entity;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
 @Entity
 @Getter
 @Setter
+@ToString
 @RequiredArgsConstructor
 @Table(name = "ROLES")
 public class RolesEntity {
@@ -30,4 +34,17 @@ public class RolesEntity {
     @OneToMany(mappedBy = "roles", fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
     @JoinColumn(referencedColumnName = "id_user")
     private Set<UserEntity> userEntitySet;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || Hibernate.getClass(this) != Hibernate.getClass(obj)) return false;
+        RolesEntity that = (RolesEntity) obj;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }

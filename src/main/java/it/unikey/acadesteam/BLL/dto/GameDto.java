ce2a1df4d4a626937619e04779e1e192bdec8718
Yanik_Dimitrov <dimitrov.yanik96@yahoo.com>
2022-03-02 15:05:48 +0100

    fix generic

--- src/main/java/it/unikey/acadesteam/BLL/dto/GameDto.java
+++ src/main/java/it/unikey/acadesteam/BLL/dto/GameDto.java
@@ -3,12 +3,18 @@ package it.unikey.acadesteam.BLL.dto;
 import it.unikey.acadesteam.DAL.entity.GameStateEntity;
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 
 public class GameDto {
     private Integer id;
     private String gameTitle;
     private String gamePath;
-    private GameStateEntity gameState;
+    private GameStateDto gameState;
+    private Set<ReportDto> reports;
+    private Set<LibraryDto> library;
+    private UserInfoDto developer;
+    //private Set<ReviewDto> reviews;
 
 }
--- src/main/java/it/unikey/acadesteam/PL/rest/GameRest.java
+++ src/main/java/it/unikey/acadesteam/PL/rest/GameRest.java
@@ -1,11 +1,18 @@
 package it.unikey.acadesteam.PL.rest;
 
+import it.unikey.acadesteam.BLL.dto.LibraryDto;
+import it.unikey.acadesteam.BLL.dto.ReportDto;
 import it.unikey.acadesteam.DAL.entity.GameStateEntity;
+
+import java.util.Set;
 
 public class GameRest {
     private Integer id;
     private String gameTitle;
     private String gamePath;
-    private GameStateEntity gameState;
+    private GameStateRest gameState;
+    private Set<ReportRest> reports;
+    //private Set<LibraryRest> library;
     //private UserInfoRest developer;
+    //private Set<ReviewRest> reviews;
 }

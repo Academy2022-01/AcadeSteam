49e7604953ad473b205fbbc5ffbb3047c3152d3d
Yanik_Dimitrov <dimitrov.yanik96@yahoo.com>
2022-03-02 11:52:16 +0100

    rest + controller + aggiornamento mapper gameState

---
+++ src/main/java/it/unikey/acadesteam/PL/controller/GameStateController.java
@@ -0,0 +1,69 @@
+package it.unikey.acadesteam.PL.controller;
+
+import it.unikey.acadesteam.BLL.dto.GameStateDto;
+import it.unikey.acadesteam.BLL.exception.NotFoundException;
+import it.unikey.acadesteam.BLL.service.impl.GameStateServiceImpl;
+import it.unikey.acadesteam.PL.mapper.GameStateRestMapper;
+import it.unikey.acadesteam.PL.rest.GameStateRest;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/gameState")
+public class GameStateController {
+
+    private final GameStateRestMapper gameStateMapper;
+    private final GameStateServiceImpl gameStateService;
+
+    @GetMapping
+    private ResponseEntity<List<GameStateRest>> findAll(){
+        List<GameStateDto> dtoState= gameStateService.getAll();
+        List<GameStateRest> restState=gameStateMapper.fromGameStateDtoToGameStateRests((GameStateDto) dtoState);
+        return ResponseEntity.ok(restState);
+    }
+
+    @GetMapping("/{id}")
+    private ResponseEntity<GameStateRest> getById(@PathVariable Integer id){
+        try {
+            GameStateDto gameState = gameStateService.getById(id);
+            return new ResponseEntity(gameStateMapper.fromGameStateDtoToGameRest(gameState),HttpStatus.OK);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    private ResponseEntity<GameStateRest> insertAuthor(@RequestBody GameStateRest stateToSave) {
+        GameStateDto stateSaved = gameStateService.insert(gameStateMapper.fromGameStateRestToGameDto(stateToSave));
+        return new ResponseEntity(gameStateMapper.fromGameStateDtoToGameRest(stateSaved),HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    private ResponseEntity<GameStateRest> updateState(@RequestBody GameStateRest stateToUpdate) {
+        try {
+            GameStateDto stateUpdated = gameStateService.update(gameStateMapper.fromGameStateRestToGameDto(stateToUpdate));
+            return new ResponseEntity(gameStateMapper.fromGameStateDtoToGameRest(stateUpdated), HttpStatus.OK);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    private  ResponseEntity<Void> deleteState(@PathVariable Integer id){
+        try {
+            gameStateService.delete(id);
+            return ResponseEntity.ok().build();
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}

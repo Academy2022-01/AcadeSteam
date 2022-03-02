57c533d94d8acf4983c753b9ed10e338b1a0c522
Hikmat Delfino <hikmatdelfino@gmail.com>
2022-03-02 12:48:11 +0100

    Rest Team 5

---
+++ src/main/java/it/unikey/acadesteam/PL/rest/LibraryRest.java
@@ -0,0 +1,12 @@
+package it.unikey.acadesteam.PL.rest;
+
+import it.unikey.acadesteam.DAL.entity.GameEntity;
+import it.unikey.acadesteam.DAL.entity.UserInfoEntity;
+
+
+public class LibraryRest {
+
+    private Integer id;
+    private GameEntity game;
+    private UserInfoEntity userInfo;
+}

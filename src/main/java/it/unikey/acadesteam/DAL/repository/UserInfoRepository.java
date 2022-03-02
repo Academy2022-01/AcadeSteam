7644b3b9601beb75882d46ba84a85e0a5bf798da
J-RADU\jatar <jataruradu@gmail.com>
2022-03-02 12:52:08 +0100

    correzione uuid -> Integer

--- src/main/java/it/unikey/acadesteam/DAL/repository/UserInfoRepository.java
+++ src/main/java/it/unikey/acadesteam/DAL/repository/UserInfoRepository.java
@@ -3,7 +3,6 @@ package it.unikey.acadesteam.DAL.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import it.unikey.acadesteam.DAL.entity.UserInfoEntity;
 
-import java.util.UUID;
 
-public interface UserInfoRepository extends JpaRepository<UserInfoEntity, UUID> {
+public interface UserInfoRepository extends JpaRepository<UserInfoEntity, Integer> {
 }

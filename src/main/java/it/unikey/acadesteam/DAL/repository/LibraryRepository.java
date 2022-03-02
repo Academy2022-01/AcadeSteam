9be5d6a5102eacf3af79aa2014814b14f2a95d4c
Roby <vaiaroberta@gmail.com>
2022-03-02 13:08:28 +0100

    added repo annotations

--- src/main/java/it/unikey/acadesteam/DAL/repository/LibraryRepository.java
+++ src/main/java/it/unikey/acadesteam/DAL/repository/LibraryRepository.java
@@ -2,7 +2,8 @@ package it.unikey.acadesteam.DAL.repository;
 
 import it.unikey.acadesteam.DAL.entity.LibraryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface LibraryRepository extends JpaRepository<LibraryEntity, Integer> {
 }
--- src/main/java/it/unikey/acadesteam/DAL/repository/UserInfoRepository.java
+++ src/main/java/it/unikey/acadesteam/DAL/repository/UserInfoRepository.java
@@ -2,7 +2,8 @@ package it.unikey.acadesteam.DAL.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import it.unikey.acadesteam.DAL.entity.UserInfoEntity;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UserInfoRepository extends JpaRepository<UserInfoEntity, Integer> {
 }

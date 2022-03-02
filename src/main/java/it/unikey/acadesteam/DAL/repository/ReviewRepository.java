680daaf7b619cce293cbaf10d0716fcb01e16009
Edoardo Carradori <edoardo.carradori.96@gmail.com>
2022-03-02 11:50:37 +0100

    modificato nome repository

---
+++ src/main/java/it/unikey/acadesteam/DAL/repository/ReviewRepository.java
@@ -0,0 +1,8 @@
+package it.unikey.acadesteam.DAL.repository;
+
+import it.unikey.acadesteam.DAL.entity.ReviewEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<ReviewEntity, Integer> {
+
+}
